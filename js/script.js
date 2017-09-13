2b31f3817129ae8db2a5cc191414ecb4cf6acbcb
Victoria <victoria-m@users.noreply.github.com>
2017-09-12 23:15:23 -0500

    Added an option to change the FPS. Markers also move as expected now.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 'use strict'
 
 // define globals
-const MARGIN = 60, WIDTH = 50, HEIGHT = 50, FPS = 20, DIRECTIONS = ['up', 'right', 'down', 'left']
-const MAX_MOVES = 1000000, RED = "red", BLUE = "blue"
-var grid, redMarker, blueMarker, results, request, numRows, numCols
+const MARGIN = 60, WIDTH = 50, HEIGHT = 50, DIRECTIONS = ['up', 'right', 'down', 'left']
+const MAX_MOVES = 1000000, RED = "red", BLUE = "blue", NUM_ROWS = 10, NUM_COLS = 10
+var fps, grid, redMarker, blueMarker, results, request, turn
 
 // set up the canvas and its context
 var canvas = document.getElementById('canvas'), context = canvas.getContext('2d')
@@ -11,44 +11,48 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
 function main() {
-  numCols = 10
-  numRows = 10
-
   // instantiate the Grid, Marker, and Results
-  grid = new Grid(numRows, numCols, HEIGHT, WIDTH, MARGIN)
-  redMarker = new Marker(0, numRows - 1, RED)
-  blueMarker = new Marker(numRows - 1, 0, BLUE)
+  grid = new Grid(NUM_ROWS, NUM_COLS, HEIGHT, WIDTH, MARGIN)
+  redMarker = new Marker(0, NUM_ROWS - 1, RED)
+  blueMarker = new Marker(NUM_ROWS - 1, 0, BLUE)
   results = new Results()
+
+  // set frames per second
+  var fpsSelection = document.getElementById('speed')
+  fps = fpsSelection.options[fpsSelection.selectedIndex].value
+
+  // red goes first
+  turn = redMarker.color
 
   // initialize the animation loop
   window.requestAnimationFrame(step)
 }
 
 // define the grid
-var Grid = function(numRows, numCols, height, width, margin) {
-  this.numRows = numRows
-  this.numCols = numCols
+var Grid = function(NUM_ROWS, NUM_COLS, height, width, margin) {
+  this.NUM_ROWS = NUM_ROWS
+  this.NUM_COLS = NUM_COLS
   this.height = height
   this.width = width
   this.margin = margin
 }
 
 Grid.prototype.draw = function(mainMarker, otherMarker) {
-  for (var i = 0; i < this.numCols; ++i) {
-    for (var j = 0; j < this.numRows; ++j) {
+  for (var i = 0; i < this.NUM_COLS; ++i) {
+    for (var j = 0; j < this.NUM_ROWS; ++j) {
       // set the default fill color:
       context.fillStyle = 'black'
 
       // enforce boundaries for the markers:
       if (mainMarker.x < 0) mainMarker.x = 0
       if (mainMarker.y < 0) mainMarker.y = 0
-      if (mainMarker.x > this.numCols - 1) mainMarker.x = this.numCols - 1
-      if (mainMarker.y > this.numRows - 1) mainMarker.y = this.numRows - 1
+      if (mainMarker.x > this.NUM_COLS - 1) mainMarker.x = this.NUM_COLS - 1
+      if (mainMarker.y > this.NUM_ROWS - 1) mainMarker.y = this.NUM_ROWS - 1
 
       if (otherMarker.x < 0) otherMarker.x = 0
       if (otherMarker.y < 0) otherMarker.y = 0
-      if (otherMarker.x > this.numCols - 1) otherMarker.x = this.numCols - 1
-      if (otherMarker.y > this.numRows - 1) otherMarker.y = this.numRows - 1
+      if (otherMarker.x > this.NUM_COLS - 1) otherMarker.x = this.NUM_COLS - 1
+      if (otherMarker.y > this.NUM_ROWS - 1) otherMarker.y = this.NUM_ROWS - 1
 
       // set the marker fill color,
       if (mainMarker.x == i && mainMarker.y == j) { context.fillStyle = mainMarker.color }
@@ -96,10 +100,10 @@ Marker.prototype.move = function() {
     if (this.y - this.steps >= 0) this.y -= this.steps
     break
   case 'right':
-    if (this.x + this.steps < grid.numCols) this.x += this.steps
+    if (this.x + this.steps < grid.NUM_COLS) this.x += this.steps
     break
   case 'down':
-    if (this.y + this.steps < grid.numRows) this.y += this.steps
+    if (this.y + this.steps < grid.NUM_ROWS) this.y += this.steps
     break
   case 'left':
     if (this.x - this.steps >= 0) this.x -= this.steps
@@ -107,14 +111,14 @@ Marker.prototype.move = function() {
   }
 
   // Step 4: check if game ended:
-  if (this.color == RED && this.x == grid.numCols - 1 && this.y == 0) {
+  if (this.color == RED && this.x == grid.NUM_COLS - 1 && this.y == 0) {
     results.gameEndReason = RED + " marker reached right corner"
     this.moving = false
     blueMarker.moving = false
   }
   else if (this.color == RED && this.history.length > MAX_MOVES) { this.moving = false }
 
-  if (this.color == BLUE && this.x == 0 && this.y == grid.numRows - 1) {
+  if (this.color == BLUE && this.x == 0 && this.y == grid.NUM_ROWS - 1) {
     results.gameEndReason = BLUE + " marker reached left corner"
     this.moving = false
     redMarker.moving = false
@@ -154,16 +158,17 @@ function step() {
   setTimeout(function() {
     request = requestAnimationFrame(step)
 
-    if (redMarker.moving) {
+    if (redMarker.moving && turn == redMarker.color) {
       context.clearRect(0, 0, canvas.width, canvas.height)
       redMarker.move()
       grid.draw(redMarker, blueMarker)
+      turn = blueMarker.color
     }
-
-    if (blueMarker.moving) {
+    else if (blueMarker.moving && turn == blueMarker.color) {
       context.clearRect(0, 0, canvas.width, canvas.height)
       blueMarker.move()
       grid.draw(blueMarker, redMarker)
+      if (redMarker.moving) { turn = redMarker.color }
     }
 
     if (!blueMarker.moving && !redMarker.moving) {
@@ -171,7 +176,17 @@ function step() {
       results.process()
       results.output(document.getElementById('results'))
     }
-  }, 1000 / FPS)
+  }, 1000 / fps)
+}
+
+function fillDropdown() {
+  const MIN_FPS = 1, MAX_FPS = 50
+  var speedDropdown = document.getElementById('speed')
+
+  // fill the dropdown list with options ranging from min to max FPS
+  for (var i = MIN_FPS; i <= MAX_FPS; ++i) {
+    if (i % 5 == 0 || i == 2) { speedDropdown.options[speedDropdown.options.length] = new Option(i, i) }
+  }
 }
 
 var Results = function() {
@@ -185,9 +200,9 @@ var Results = function() {
 
 Results.prototype.process = function() {
   // build the cell touches grid, fill it with 0's
-  for (var i = 0; i < grid.numRows; ++i) {
+  for (var i = 0; i < grid.NUM_ROWS; ++i) {
     this.cellTouches[i] = new Array()
-    for (var j = 0; j < grid.numCols; ++j) { this.cellTouches[i][j] = 0 }
+    for (var j = 0; j < grid.NUM_COLS; ++j) { this.cellTouches[i][j] = 0 }
   }
 
   // increment cell touches grid's indices for every set of marker coordinates
@@ -207,7 +222,7 @@ Results.prototype.process = function() {
     }
   }
 
-  this.avgTouches = sum / (grid.numRows * grid.numCols)
+  this.avgTouches = sum / (grid.NUM_ROWS * grid.NUM_COLS)
 }
 
 Results.prototype.toHtml = function() {
